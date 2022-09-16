cc14f622045f00bf4bcd19042bfb8c69c4534a4b
claudioas <claudioasepulvedav@gmail.com>
2022-09-16 18:58:25 -0300

    Challenge Promise

--- src/callback/index.js
+++ src/callback/index.js
@@ -2,13 +2,13 @@ function sum(num1, num2) {
     return num1 + num2;
 }
 
-function calc(num1, num2, sumar) {
-    return sumar(num1, num2)
+function calc(num1, num2, callback) {
+    return callback(num1, num2)
 }
 
 console.log(calc(2, 2, sum));
 
-setTimeout(function () {
+setTimeout( () => {
     console.log('Hello JavaScript')
 }, 5000);
 
